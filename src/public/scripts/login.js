c3721e79d4f7f0a833e146efe79cb8472b92b520
u23608821 <u23608821@tuks.co.za>
2025-05-20 12:32:53 +0200

    Updated file structure. Seperated html, css and js into own files

---
+++ src/public/scripts/login.js
@@ -0,0 +1,23 @@
+function getCookie(name) {
+    let cname = name + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i].trim();
+        if (c.indexOf(cname) === 0) {
+            return c.substring(cname.length, c.length);
+        }
+    }
+    return "";
+}
+
+function applySavedTheme() {
+    const savedTheme = getCookie("theme");
+    if (savedTheme === "dark") {
+        document.body.classList.add("dark");
+    } else {
+        document.body.classList.remove("dark");
+    }
+}
+
+window.addEventListener("load", applySavedTheme);
